8c16228de7a965eecd857b2100132f9c202d0705
Alexander Aryanfar <alexander.aryanfar@gmail.com>
2024-10-31 12:16:42 -0400

    commit
    2024

---
+++ Jokes/Controllers/Jokes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Jokes.Controllers
+{
+	public class Jokes
+	{
+		public Jokes()
+		{
+		}
+		public int JokesID { get; set; }
+
+		public string JokesQuestions { get; set; }
+
+		public string JokesAnsewrs { get; set; }
+	}
+}
+
